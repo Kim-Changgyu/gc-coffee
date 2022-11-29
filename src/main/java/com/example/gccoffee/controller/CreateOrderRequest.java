c5a59779d2f4d9f00eb2125a7a108eed5f5a85a2
Kim Changgyu <kimc980106@naver.com>
2022-11-29 16:28:48 +0900

    Feat: 주문 기능 구현

---
+++ src/main/java/com/example/gccoffee/controller/CreateOrderRequest.java
@@ -0,0 +1,13 @@
+package com.example.gccoffee.controller;
+
+import com.example.gccoffee.model.OrderItem;
+
+import java.util.List;
+
+public record CreateOrderRequest(
+        String email,
+        String address,
+        String postcode,
+        List<OrderItem> orderItems
+) {
+}
